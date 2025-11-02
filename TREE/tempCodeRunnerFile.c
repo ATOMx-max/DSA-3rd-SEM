9053130c1f5c42443eb9f591006ea7711a58dfe7
ATOMx-max <sjs36931@gmail.com>
2025-11-02 10:50:52 +0530

    tree function

---
+++ TREE/tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+    temp=searchR(root,200);
--- TREE/tree.c
+++ TREE/tree.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<limits.h>
 struct node{
     struct node* lchild;
     int data;
@@ -77,6 +78,83 @@ void insert(int key)
         parent->rchild=create(key);
     }
 }
+struct node *searchR(struct node *r,int key)
+{
+    if(r==NULL)return NULL;
+    else if (key==r->data)return r;
+    else if (key<r->data)
+    {
+        return searchR(r->lchild,key);
+    }
+    else
+    {
+        return searchR(r->rchild,key);
+    }
+}
+int search(int key)
+{
+    if(root==NULL)return 0;
+    struct node *temp=root;
+    while(temp!=NULL)
+    {
+        if(key==temp->data)return 1;
+        else if(key<temp->data)
+        {
+            temp=temp->lchild;
+        }
+        else
+        {
+            temp=temp->rchild;
+        }
+    }
+    return 0;
+}
+int smallestR(struct node *r)
+{
+    if(r==NULL)return INT32_MAX;
+    else if(r->lchild==NULL)return r->data;
+    else
+    {
+        return smallestR(r->lchild);
+    }
+}
+int smallest(struct node *r)
+{
+    if (r==NULL)return INT16_MAX;
+    while(r!=NULL)
+    {
+        if(r->lchild==NULL)
+        {
+            return r->data;
+        }
+        else{
+            r=r->lchild;
+        }
+    }
+}
+int largest(struct node *r)
+{
+    if (r==NULL)return INT_MIN;
+    else if(r->rchild==NULL) return r->data;
+    else
+        return largest(r->rchild);
+}
+int total_node(struct node *r)
+{
+    if(r==NULL)return 0;
+    else{
+        return total_node(r->lchild)+total_node(r->rchild)+1;
+    }
+}
+int In_node(struct node *r)
+{
+    if(r==NULL)return 0;
+    else if (r->lchild!=NULL && r->rchild!=NULL)
+    {
+        return(In_node(r->lchild)+In_node(r->rchild)+1);
+    }
+    return In_node(r->lchild)+In_node(r->rchild);
+}
 int main()
 {
     insert(50);
@@ -87,5 +165,17 @@ int main()
     insert(45);
     preorder(root);
 
+    struct node *temp=searchR(root,90);
+    printf("\n%u",temp);
+    temp=searchR(root,200);
+    printf("\n%u",temp);
+    int ans=search(45);
+    printf("\n%d",ans);
+    printf("\n%d",smallestR(root));
+    printf("\n%d",smallest(root));
+    printf("\n%d",largest(root));
+    printf("\n%d",total_node(root));
+    printf("\n%d",In_node(root));
 
+    return 0;
 }
